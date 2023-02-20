4957bff283b0cf604c35168d0aa3f1c36c06edd8
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-20 01:08:50 -0300

    Create Threads.java

---
+++ Exercicios/Exercicio 13/src/Thread/Threads.java
@@ -0,0 +1,18 @@
+public class Threads {
+	
+	public static void main(String[] args) {
+		// Thread Atual
+		Thread t = Thread.currentThread();
+		System.out.println(t.getName());
+		
+		
+		// Nova Thread
+		Thread t0 = new Thread(new MeuRunnable());
+		//t0.run(); // apenas executando na mesma thread
+		t0.start(); // executando em uma nova thread
+		
+		// Runnable com Lambda
+		Thread t1 = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+		t1.start();
+	}	
+}
